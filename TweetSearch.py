7c780b4b590ccf2111cdb68bfee1f7f71b9c99c2
Sahithi <sahithiaelecloud@gmail.com>
2024-11-15 13:07:27 +0530

    Initial

---
+++ TweetSearch.py
@@ -0,0 +1,19 @@
+# importing and initialising exa with API key S
+from exa_py import Exa
+exa = Exa('4c1199ac-a5da-446e-a911-258e09d57665')
+
+# Creating a Variable called  "query" , which will hold the response to the input 
+
+query = input('Search here: ')
+
+response = exa.search(
+  query,
+  num_results=5,
+  type='keyword',
+  include_domains=['https://x.com/'],
+)
+
+for result in response.results:
+  print(f'Title: {result.title}')
+  print(f'URL: {result.url}')
+  print()
